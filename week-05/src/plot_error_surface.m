a14297054fdb0681fef0beaad9a9a0f675e21477
Daniel Fernandes Martins <daniel.fernandes@corp.globo.com>
2013-11-02 01:29:52 -0200

    New plots.

--- week-05/src/plot_error_surface.m
+++ week-05/src/plot_error_surface.m
@@ -6,38 +6,25 @@ clear all;
 close all;
 clc;
 
-% Plots a given nonlinear transformation
-function plottrans (spc, f, tit)
-  x = y = linspace (spc(1), spc(2));
-
-  [xx, yy] = meshgrid (x', y');
-
-  contour (xx, yy, arrayfun (f, xx, yy));
-  title (tit);
-
-  xlabel ("w_1");
-  ylabel ("w_2");
-end
-
 % Axis limits
 limits = [-1 1];
 
 % Plots each proposed error surface
 
 % h = figure (1);
-% plottrans (limits, @ (w1, w2) w2^2, "w_1 = 0, w_2 > 0");
+% ploterrfunc (limits, @ (w) w(2)^2, "w_1 = 0, w_2 > 0");
 
 % h = figure (2);
-% plottrans (limits, @ (w1, w2) w1^2, "w_1 > 0, w_2 = 0");
+% ploterrfunc (limits, @ (w) w(1)^2, "w_1 > 0, w_2 = 0");
 
 % h = figure (3);
-% plottrans (limits, @ (w1, w2) w1^2 + w2^2,  "w_1 > 0, w_2 > 0");
+% ploterrfunc (limits, @ (w) w(1)^2 + w(2)^2,  "w_1 > 0, w_2 > 0");
 
 h = figure (4);
-plottrans (limits, @ (w1, w2) -w1^2 + w2^2, "w_1 < 0, w_2 > 0");
+ploterrfunc (limits, @ (w) -w(1)^2 + w(2)^2, "w_1 < 0, w_2 > 0");
 
 % h = figure (5);
-% plottrans (limits, @ (w1, w2) w1^2 + -w2^2, "w_1 > 0, w_2 < 0");
+% ploterrfunc (limits, @ (w) w(1)^2 + -w(2)^2, "w_1 > 0, w_2 < 0");
 
 % Uncomment the following line in order to save the plot to a PNG file
 % saveplot (h, 4, 3, "../img/plot_error_surface.png");
--- week-05/src/q07.m
+++ week-05/src/q07.m
@@ -5,21 +5,6 @@ addpath ("./common");
 clear all;
 clc;
 
-% Function that defines the error surface
-errfunc = @ (w) (w(1) * exp (w(2)) - 2*w(2) * exp (-w(1)))^2;
-
-% Function that computes the gradient for the error surface
-function nw = errfuncgrad (w)
-  u = w(1);
-  v = w(2);
-
-  % Partial derivative with respect to u and v, respectively
-  du = 2 * exp (-2*u) * (u*exp (u+v) - 2*v) * (exp (u+v) + 2*v);
-  dv = 2 * exp (-2*u) * (u*exp (u+v) - 2) * (u*exp (u+v) - 2*v);
-
-  nw = [du; dv];
-end
-
 % Learning rate
 eta = 0.1;
 
@@ -33,6 +18,6 @@ iters = 15;
 w= ones (2, 1);
 
 % Runs coordinate descent
-[w, epoch] = cgd (w, eta, errfunc, @errfuncgrad, errprec, iters);
+[w, epoch] = cgd (w, eta, @errfunc, @errfuncgrad, errprec, iters);
 
 fprintf ("Ein: %f\n", errfunc (w));
