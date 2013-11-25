ea2ea72edfd9cacf78d06128386781d67a663094
Daniel Fernandes Martins <daniel.fernandes@corp.globo.com>
2013-11-24 22:54:05 -0200

    Improved precision of separating hyperplane.

--- week-08/src/common/plotboundary.m
+++ week-08/src/common/plotboundary.m
@@ -2,8 +2,8 @@
 function plotboundary (model, X)
   l = 0.1;
 
-  xplot = linspace(min(X(:,1))-l, max(X(:,1))+l, 500)';
-  yplot = linspace(min(X(:,2))-l, max(X(:,2))+l, 500)';
+  xplot = linspace(min(X(:,1))-l, max(X(:,1))+l, 1000)';
+  yplot = linspace(min(X(:,2))-l, max(X(:,2))+l, 1000)';
 
   axis ([min(X(:,1)) max(X(:,1)) min(X(:,2)) max(X(:,2))]);
 
@@ -16,8 +16,8 @@ function plotboundary (model, X)
   [predicted_label accuracy decision_values] = svmpredict (zeros (length (dd), 1), dd, model, '-q');
 
   neg = find (predicted_label == -1);
-  plot (dd(neg,1), dd(neg,2), 's', 'color', negcolor, 'MarkerEdgeColor', negcolor, 'MarkerFaceColor', negcolor);
+  plot (dd(neg,1), dd(neg,2), 's', 'color', negcolor, 'MarkerSize', 1, 'MarkerEdgeColor', negcolor, 'MarkerFaceColor', negcolor);
 
   pos = find(predicted_label == 1);
-  plot(dd(pos,1), dd(pos,2), 's', 'color', poscolor, 'MarkerEdgeColor', poscolor, 'MarkerFaceColor', poscolor);
+  plot(dd(pos,1), dd(pos,2), 's', 'color', poscolor, 'MarkerSize', 1, 'MarkerEdgeColor', poscolor, 'MarkerFaceColor', poscolor);
 end
